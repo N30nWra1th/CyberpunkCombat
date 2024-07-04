0d099e86a0d1db85f63f841df9f5412930b150a2
Czeh Gabor <czeh.gabor@protonmail.com>
2024-07-04 21:45:29 +0200

    Database created, writing logic for base methods

--- src/main/java/com/own/cyberpunk/repository/FighterRepository.java
+++ src/main/java/com/own/cyberpunk/repository/FighterRepository.java
@@ -1,8 +1,9 @@
 package com.own.cyberpunk.repository;
 
+import com.own.cyberpunk.domain.Fighter;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FighterRepository extends CrudRepository<Character, Long> {
+public interface FighterRepository extends CrudRepository<Fighter, Long> {
 }
